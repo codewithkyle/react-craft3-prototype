cf0ae976b9b8eeada38f206acb431a63a0a94ae7
Kyle Andrews <kylea@page.works>
2019-08-09 19:02:22 -0400

    Styles sidebar

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 
+import './sidebar.scss';
+
+import { SignoutButton } from './SignoutButton';
+
 export class Sidebar extends Component
 {
 	render()
 	{
 		return(
 			<aside>
-				<h1>Sidebar</h1>
+				<div className="user-info">
+					<div className="user-info_icon">
+						<span>KA</span>
+					</div>
+					<span className="user-info_name">Kyle Andrews</span>
+					<span className="user-info_email">kylea@page.works</span>
+				</div>
+				<nav>
+					<button className="is-active">Dashboard</button>
+					<button>Courses</button>
+					<button>Wishlist</button>
+					<button>Teach</button>
+					<button>Settings</button>
+				</nav>
+				<SignoutButton></SignoutButton>
 			</aside>
 		);
 	}
---
+++ src/SignoutButton.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class SignoutButton extends Component
+{
+	render()
+	{
+		return(
+			<button className="user-signout">Sign Out</button>
+		);
+	}
+}
